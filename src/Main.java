d9d162dfad768dc25d54fde61af4472c69147aaf
JekaterinaL <jekaterina.losseva@itcollege.ee>
2016-10-20 23:08:35 +0300

    byte, int, double

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main (String args[]) {
-        System.out.println("Alustan harjutamist");
+        double a = 8.76;
+        int b = 2;
+        double c = a + b;
+        double d = a - b;
+        double e = a * b;
+        double f = a / b;
+        double g = Math.pow(a, b); // a astmes b
+        System.out.println("c = " + c +"; d = " + d +"; e = " + e +"; f = " + f +"; g = " + g);
+        System.out.println("g = " + Math.round(g));
+        }
     }
-}
